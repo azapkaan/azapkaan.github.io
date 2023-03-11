d485bf954a3e68288aef25adbb1c16935f390576
azapkaan <76071456+azapkaan@users.noreply.github.com>
2023-03-11 01:31:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,33 @@ var saatte_toplam_deger = 0;
 var gunde_toplam_deger = 0;
 var kalan_gun = kalan_sayi.value;
 
+///sayi format
+function Sayi_cevirme(num) {
+  num = num.toString().replace(/[^0-9.]/g, "");
+
+  let si = [
+    { v: 1, s: "FF" },
+    { v: 1e3, s: "GG" },
+    { v: 1e6, s: "HH" },
+    { v: 1e9, s: "B" },
+    { v: 1e12, s: "T" },
+    { v: 1e15, s: "P" },
+    { v: 1e18, s: "E" },
+    { v: 1e21, s: "E" },
+  ];
+  let index;
+  for (index = si.length - 1; index > 0; index--) {
+    if (num >= si[index].v) {
+      break;
+    }
+  }
+  return (
+    (num / si[index].v).toFixed(1).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") +
+    si[index].s
+  );
+}
+///sayi format
+
 girdi.addEventListener("change", (even) => {
   saniye_basina_deger = even.target.value;
 });
@@ -26,23 +53,13 @@ hesapla_butonu.addEventListener("click", function () {
   saatte_toplam_deger = dakikada_toplam_deger * 60;
   gunde_toplam_deger = saatte_toplam_deger * 24;
 
-  dakikada.innerHTML =
-    `Dakikada: ${dakikada_toplam_deger.toLocaleString(
-      "en-us",
-      parseInt(dakikada_toplam_deger)
-    )}` + "f";
+  dakikada.innerHTML = `Dakikada: ${Sayi_cevirme(dakikada_toplam_deger)}`;
 
-  saatte.innerHTML = `Saatte: ${saatte_toplam_deger.toLocaleString(
-    "en-US",
-    parseInt(saatte_toplam_deger)
-  )}`;
+  saatte.innerHTML = `Saatte: ${Sayi_cevirme(saatte_toplam_deger)}`;
 
-  gunde.innerHTML = `Günde: ${gunde_toplam_deger.toLocaleString(
-    "en-us",
-    parseInt(gunde_toplam_deger)
-  )}`;
+  gunde.innerHTML = `Günde: ${Sayi_cevirme(gunde_toplam_deger)}`;
 
-  girdigin_sayi.innerHTML = saniye_basina_deger + "f";
+  girdigin_sayi.innerHTML = Sayi_cevirme(saniye_basina_deger);
   kalan_gun_degeri = kalan_gun / gunde_toplam_deger;
   kalan.innerHTML = `Kalan gün: ${parseInt(kalan_gun_degeri)}`;
 });
