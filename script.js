7a34aee515fb9d2396fe3150aad8535dbe0c7127
azapkaan <76071456+azapkaan@users.noreply.github.com>
2023-04-08 18:23:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ function Sayi_cevirme(num) {
   num = num.toString().replace(/[^0-9.]/g, "");
 
   let si = [
-    { v: 1, s: "FF" },
-    { v: 1e3, s: "GG" },
-    { v: 1e6, s: "HH" },
+    { v: 1, s: "DD" },
+    { v: 1e3, s: "EE" },
+    { v: 1e6, s: "FF" },
     { v: 1e9, s: "B" },
     { v: 1e12, s: "T" },
     { v: 1e15, s: "P" },
